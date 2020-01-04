0b940ea8ed45a66bdbb819e6dd5dba37d18702e8
FaadLi <mzfadlii@gmail.com>
2020-01-04 19:02:42 +0700

    Update footer.php

--- member/templates/footer.php
+++ member/templates/footer.php
@@ -1,7 +1,8 @@
     
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     
-    <script type="text/javascript" src="../js/jquery.js"></script>
+
     <script type="text/javascript" src="../js/materialize.min.js"></script>
     <script type="text/javascript" src="../js/dataTable.min.js"></script>
 
